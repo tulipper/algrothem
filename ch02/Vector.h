c36b8bac891f1f72b3f3aeb8c98a70f579215c15
tulipper_local <1324227851@qq.com>
2019-12-24 21:44:23 +0800

    加入插入，移除遍历操作

--- ch02/Vector.h
+++ ch02/Vector.h
@@ -116,3 +116,56 @@ template <typename T> T& Vector<T> :: operator[] (Rank r) const {
     return _elem[r];
 }
 
+//区间置乱接口unsrot
+template <typename T > void Vector<T>::unsort(Rank lo, Rank hi) {
+    T* V = _elem + lo;
+    for(int i = hi - lo; i > 0; i --) {
+        swap(V[i-1], V[rand() % i]);
+    }
+}
+
+//无序向量元素查找接口find()
+template <typename T> Rank Vector<T>::find(T const &e, Rank lo, Rank hi) const {
+    while((lo < hi--) && (_elem[hi] != e));
+    return hi;
+}
+//向量元素插入接口Insert()
+template <typename T> Rank Vector<T>::insert(Rank r, T const &e) {
+    expand();
+    for(int i = _size; i > r; i --) _elem[i] =  _elem[i-1];
+    _elem[r] = e; _size ++;
+    return r;
+}
+//向量区间删除接口
+template <typename T> int Vector<T> :: remove(Rank lo, Rank hi) {
+    if(lo == hi) return 0;
+    while(hi < _size ) _elem[lo++] = _elem[hi++];
+    _size = lo;
+    shrink();
+    return hi-lo;
+}
+
+//向量氮元素删除接口
+template <typename T> T Vector<T> :: remove(Rank r) {
+    T e = elem[r];
+    remove(r, r+1);
+    return e;
+}
+//无序向量清楚重复元素接口 deduplicate()
+template <typename T> int Vector<T> :: deduplicate() {
+    int oldSize = _size;
+    Rank i = 1;
+    while(i < _size)
+        (find(0, i, _elem[i])) < 0 ? i ++ : remove(i);
+    return oldSize = _size;
+}
+//遍历接口
+
+template <typename T> void  Vector<T> :: traverse(void(* visit) (T&)) {
+    for(int i = 0; i < _size; i ++) visit(_elem[i]);
+}
+
+template <typename T> template <typename VST>
+void Vector<T> :: traverse(VST &visit) {
+    for (int i = 0; i < _size; i ++) visit(_elem[i]);
+}
